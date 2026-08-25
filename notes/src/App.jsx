a305e64cd905da2db11c8d499a71cf084bc4122e
aada <aadaappelsiini@gmail.com>
2026-08-25 19:48:31 +0300

    notes filter

--- notes/src/App.jsx
+++ notes/src/App.jsx
@@ -1,10 +1,12 @@
 import NoteList from "./components/NoteList"
 import NoteForm from "./components/NoteForm"
+import VisibilityFilter from "./components/VisibilityFilter"
 
 const App = () => {
   return (
     <div>
       <NoteForm />
+      <VisibilityFilter />
       <NoteList />
     </div>
   )

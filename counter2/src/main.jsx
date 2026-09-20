6180d7c544d22df2e97fe972f978f5e2387bc7f1
aada <aadaappelsiini@gmail.com>
2026-09-20 14:18:44 +0300

    counter with react context

---
+++ counter2/src/main.jsx
@@ -0,0 +1,10 @@
+import { createRoot } from 'react-dom/client'
+
+import App from './App.jsx'
+import { CounterContextProvider } from './CounterContext'
+
+createRoot(document.getElementById('root')).render(
+  <CounterContextProvider>
+    <App />
+  </CounterContextProvider>
+)
